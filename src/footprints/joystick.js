51114ebbc1fa0bbed08c40bb1a87c58303af50dd
Spoofy <alec.mccaffrey@gmail.com>
2022-06-16 13:20:03 -0400

    parametric rectangle fill for promicro

--- src/footprints/joystick.js
+++ src/footprints/joystick.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     nets: {
-        X: undefined,
-        Y: undefined,
+        A: undefined,
+        B: undefined,
         VCC: 'VCC',
         GND: 'GND'
     },
@@ -18,21 +18,22 @@ module.exports = {
             ${'' /* footprint reference */}
             (fp_text reference "${p.ref}" (at 0 0.5) (layer F.SilkS) 
                 ${p.ref_hide} (effects (font (size 1 1) (thickness 0.15))))
+            (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
 
             ${'' /* component outline */}
             (fp_rect (start -8.75 -8.75) (end 8.75 8.75) (layer "Margin") (width 0.12) (fill none)
 
-            ${'' /* legs */}
-            (pad "" np_thru_hole circle (at -6 -6) (size 0 0) (drill 1.5) (layers F&B.Cu *.Mask)
-            (pad "" np_thru_hole circle (at 6 6) (size 0 0) (drill 1.5) (layers F&B.Cu *.Mask)
-
             ${'' /* pin names */}
-            (pad "GND" thru_hole circle (at 8.6 -2) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.GND.str})
-            (pad "GND" thru_hole circle (at -2 -8.6) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.GND.str})
-            (pad "VCC" thru_hole circle (at 8.6 2) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.VCC.str})
-            (pad "VCC" thru_hole circle (at 2 -8.6) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.VCC.str})
-            (pad "X" thru_hole circle (at 0 -8.6) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.X.str})
-            (pad "Y" thru_hole circle (at 8.6 0) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.Y.str})
+            (pad GND thru_hole circle (at 8.6 -2) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.GND.str})
+            (pad GND thru_hole circle (at -2 -8.6) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.GND.str})
+            (pad VCC thru_hole circle (at 8.6 2) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.VCC.str})
+            (pad VCC thru_hole circle (at 2 -8.6) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.VCC.str})
+            (pad X thru_hole circle (at 0 -8.6) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.A.str})
+            (pad Y thru_hole circle (at 8.6 0) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.B.str})
+
+            ${'' /* legs */}
+            (pad "" np_thru_hole circle (at -6 -6) (size 0 0) (drill 1.5) (layers *.Cu *.Mask)
+            (pad "" np_thru_hole circle (at 6 6) (size 0 0) (drill 1.5) (layers *.Cu *.Mask)
         )
     `
 }

7f893f8fb7d47a70c15aa13a0512edd8f4bbeeac
Spoofy <alec.mccaffrey@gmail.com>
2022-06-21 19:42:49 -0400

    joystick footprint fix and flip testing

--- src/footprints/joystick.js
+++ src/footprints/joystick.js
@@ -13,7 +13,7 @@ module.exports = {
     body: p => `
         (module joystick (layer F.Cu)
 
-            ${p.at}
+            ${p.at /* parametric position */}
 
             ${'' /* footprint reference */}
             (fp_text reference "${p.ref}" (at 0 0.5) (layer F.SilkS) 
@@ -21,19 +21,19 @@ module.exports = {
             (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
 
             ${'' /* component outline */}
-            (fp_rect (start -8.75 -8.75) (end 8.75 8.75) (layer "Margin") (width 0.12) (fill none)
+            (fp_rect (start -8.75 -8.75) (end 8.75 8.75) (layer Dwgs.User) (width 0.12) (fill none))
 
             ${'' /* pin names */}
             (pad GND thru_hole circle (at 8.6 -2) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.GND.str})
             (pad GND thru_hole circle (at -2 -8.6) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.GND.str})
             (pad VCC thru_hole circle (at 8.6 2) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.VCC.str})
             (pad VCC thru_hole circle (at 2 -8.6) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.VCC.str})
-            (pad X thru_hole circle (at 0 -8.6) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.A.str})
-            (pad Y thru_hole circle (at 8.6 0) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.B.str})
+            (pad A thru_hole circle (at 0 -8.6) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.A.str})
+            (pad B thru_hole circle (at 8.6 0) (size 1.524 1.524) (drill 0.762) (layers *.Cu *.Mask) ${p.net.B.str})
 
             ${'' /* legs */}
-            (pad "" np_thru_hole circle (at -6 -6) (size 0 0) (drill 1.5) (layers *.Cu *.Mask)
-            (pad "" np_thru_hole circle (at 6 6) (size 0 0) (drill 1.5) (layers *.Cu *.Mask)
+            (pad "" np_thru_hole circle (at -6 -6) (size 0 0) (drill 1.5) (layers *.Cu *.Mask))
+            (pad "" np_thru_hole circle (at 6 6) (size 0 0) (drill 1.5) (layers *.Cu *.Mask))
         )
     `
 }
